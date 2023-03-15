7365b2d15941b2233e4ee28f092e55e2f88a74de
koekoesh <lotfizizaoui@gmail.com>
2023-03-15 12:03:49 +0100

    getters en setters

--- source/Main.java
+++ source/Main.java
@@ -11,8 +11,29 @@ class Options{
         this.type = type;
         this.description = description;
         this.environmentDiscount = environmentDiscount;
-        dikke lul;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setEnvironmentDiscount(Boolean environmentDiscount) {
+        this.environmentDiscount = environmentDiscount;
+    }
+
+
 }
 
 
