8c455e61342f67054ea35b88eeb7030eddf70b8f
koekoesh <lotfizizaoui@gmail.com>
2023-03-15 11:58:33 +0100

    ewa ja

--- source/Main.java
+++ source/Main.java
@@ -1,2 +1,19 @@
+class Options{
+    String name;
+    Double price;
+    String type;
+    String description;
+    Boolean environmentDiscount;
+
+    Options(String name, Double price, String type, String description, Boolean environmentDiscount){
+        this.name = name;
+        this.price = price;
+        this.type = type;
+        this.description = description;
+        this.environmentDiscount = environmentDiscount;
+    }
+}
+
+
 public class Main {
 }
