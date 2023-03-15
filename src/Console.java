9829c2ca1d14a513544d517cdeda4f2fb0608b5b
Thom <22082476@student.hhs.nl>
2023-03-15 15:01:39 +0100

    edit

--- src/Console.java
+++ src/Console.java
@@ -6,21 +6,29 @@ public class Console {
     public static void welcome(){
         //standaard welcome tekst
         System.out.printf("Welcome by shipflex%n");
-        commandsText();
+        commandsStart();
     }
 
-    public static void commandsText(){
-        System.out.printf("Commands: \'optieLijst\', \'offerteMaken\', \'optieToevoegen\', \'OptieVerwijderen\', \'stop\'%n");
-        System.out.println();
+    public static void commandsStart(){
+        System.out.println("Commands: \'start\', \'onderhoud\', \'stop\'");
+        printInputText();
+    }
+    public static void printInputText(){
         System.out.printf("tik een command in:%n");
     }
+    public static void commandsOnderhoud(){
+        System.out.printf("Commands: \'optieLijst\', \'offerteMaken\', \'optieToevoegen\', \'OptieVerwijderen\',\'back\', \'stop\'%n");
+        printInputText();
+    }
+
+
 
     public static String readIn(){
         return in.nextLine();
     }
 
-    public static boolean terminate(String in){
-        if(in.equals("Stop")|| in.equals("stop")){
+    public static boolean terminate(String input){
+        if(input.toLowerCase().equals("stop")){
             return true;
         }else {
             return false;
@@ -35,8 +43,14 @@ public class Console {
     if(terminate(input)){
         break;
     }else {
-        checkInputCommand(input);
-        commandsText();
+        if(getBooStart(input)){
+
+        } else if (getBooOnderhoud(input)) {
+            while(true){
+
+            }
+        }
+
 
     }
     }
@@ -58,20 +72,28 @@ public class Console {
 
     }
 
+    public static boolean getBooOnderhoud(String input){
+        return input.toLowerCase().equals("onderhoud");
+    }
+
+    public static boolean getBooStart(String input){
+        return input.toLowerCase().equals("start");
+    }
+
     public static boolean getBooOptieLijst(String input){
-        return input.equals("optieLijst") || input.equals("optielijst");
+        return input.toLowerCase().equals("optielijst");
     }
 
     public static boolean getBooOptieToevoegen(String input){
-        return in.equals("optieToevoegen") || in.equals("optietoevoegen");
+        return input.toLowerCase().equals("optietoevoegen");
     }
 
     public static boolean getBooOptieVerwijderen(String input){
-        return input.equals("optieVerwijderen") || input.equals("optieverwijderen");
+        return input.toLowerCase().equals("optieverwijderen");
     }
 
     public static boolean getBooOfferteMaken(String input){
-        return input.equals("offerteMaken") || input.equals("offertemaken");
+        return input.toLowerCase().equals("offertemaken");
     }
 
 
