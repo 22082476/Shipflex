b9f0510944def6fd71f862682727bebbc220e6e6
Thom <22082476@student.hhs.nl>
2023-03-15 11:56:25 +0100

    test added console.java

---
+++ source/Console.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Console {
+    private Scanner in = new Scanner(System.in);
+
+    public void welcome(){
+        //standaard welcome tekst
+
+    }
+
+    public void commandsText(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
+
