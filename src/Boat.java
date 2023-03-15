15dff3eef8a454753c60ca96e4887addeb965ac6
koekoesh <lotfizizaoui@gmail.com>
2023-03-15 14:02:19 +0100

    added constructor en arraylist

--- src/Boat.java
+++ src/Boat.java
@@ -15,4 +15,45 @@ public class Boat {
     ArrayList<Option>options = new ArrayList<>();
     ArrayList<Option>extraOptions = new ArrayList<>();
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBaseprice(Double baseprice) {
+        this.baseprice = baseprice;
+    }
+
+    public void setOptions(ArrayList<Option> options) {
+        this.options = options;
+    }
+
+    public void setExtraOptions(ArrayList<Option> extraOptions) {
+        this.extraOptions = extraOptions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Double getBaseprice() {
+        return baseprice;
+    }
+
+    public ArrayList<Option> getOptions() {
+        return options;
+    }
+
+    public ArrayList<Option> getExtraOptions() {
+        return extraOptions;
+    }
+
+
 }
