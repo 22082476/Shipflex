a1252c3357cb9f42311688628c955863cf5df350
Thom <22082476@student.hhs.nl>
2023-03-15 12:04:55 +0100

    welcome and cammand text

--- source/Console.java
+++ source/Console.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Console {
     private Scanner in = new Scanner(System.in);
 
-    public void welcome(){
+    public static void welcome(){
         //standaard welcome tekst
-
+        System.out.printf("Welcome by shipflex%n");
+        commandsText();
     }
 
-    public void commandsText(){
-
+    public static void commandsText(){
+        System.out.printf("Commands: \'optieLijst\', \'offerteMaken\', \'optieToevoegen\', \'OptieVerwijderen\'%n");
+        System.out.println();
+        System.out.printf("tik een command in:%n");
     }
 
     public static void main(String[] args){
-
+    welcome();
     }
 
 
