859455686a6e3d8c2a265ed6c6c1886d3dd0210c
koekoesh <lotfizizaoui@gmail.com>
2023-03-15 13:27:27 +0100

    added constructor en arraylist

--- src/Boat.java
+++ src/Boat.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Boat {
+
+    String name;
+    String type;
+
+    Boat(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    ArrayList<Option>options = new ArrayList<>();
+    ArrayList<Option>extraOptions = new ArrayList<>();
+
 }
