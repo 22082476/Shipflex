242c2db019e377697dce2943cff625b00c5c3c2d
Thom <22082476@student.hhs.nl>
2023-03-22 11:58:05 +0100

    Add Setters for Quote.java

--- src/Quote.java
+++ src/Quote.java
@@ -15,4 +15,23 @@ public class Quote {
     this.customer = customer;
     }
 
+    public void setCompanyShipbuild(Company companyShipbuild) {
+        this.companyShipbuild = companyShipbuild;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public void setQuoteDate(String quoteDate) {
+        QuoteDate = quoteDate;
+    }
+
+    public void setBoat(Boat boat) {
+        this.boat = boat;
+    }
 }
