8544a2165c9e20194ea86f94101a3ab4475d7a58
Thom <22082476@student.hhs.nl>
2023-03-22 11:55:50 +0100

    Add constructor for Quote.java

--- src/Quote.java
+++ src/Quote.java
@@ -1,3 +1,18 @@
 public class Quote {
+    private Company companyShipbuild;
+
+    private Customer customer;
+    private String Date;
+    private String QuoteDate;
+    private Boat boat;
+
+    public Quote(Company companyShipbuild){
+        this.companyShipbuild = companyShipbuild;
+    }
+
+    public Quote(Company companyShipbuild, Customer customer){
+    this(companyShipbuild);
+    this.customer = customer;
+    }
 
 }
