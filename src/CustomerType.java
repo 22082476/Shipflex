e86395f5383e0ae55b59f28d7c93de745a448652
22013024-Romano <22013024@student.hhs.nl>
2023-03-22 11:27:25 +0100

    1

--- src/CustomerType.java
+++ src/CustomerType.java
@@ -1,18 +1,12 @@
-class CustomerType {
+class Customer {
     protected String name;
     protected String street;
     protected String postcode;
     protected String city;
     protected int houseNumber;
 
-}
 
-class BusinessCustomer extends CustomerType{
-    // class company moet nog aangemaakt worden
-
-    Company company;
-
-    public BusinessCustomer(String name, String street, String postcode, String city, int houseNumber, Company company){
+    public Customer(String name, String street, String postcode, String city, int houseNumber) {
         this.name = name;
         this.street = street;
         this.postcode = postcode;
@@ -20,4 +14,81 @@ class BusinessCustomer extends CustomerType{
         this.houseNumber = houseNumber;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+}
+
+class BusinessCustomer extends Customer{
+    // class company moet nog aangemaakt worden
+    private Company company;
+
+
+    public BusinessCustomer(String name, String street, String postcode, String city, int houseNumber,Company company){
+        super(name,street,postcode,city,houseNumber);
+        this.company = company;
+
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+}
+
+class GovermentCustomer extends Customer {
+
+    private String ministy;
+
+    public GovermentCustomer (String name, String street, String postcode, String city, int houseNumber, String ministy){
+        super(name, street, postcode, city, houseNumber);
+        this.ministy = ministy;
+    }
+
+    public String getMinisty() {
+        return ministy;
+    }
+
+    public void setMinisty(String ministy) {
+        this.ministy = ministy;
+    }
 }
