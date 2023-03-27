d8c2787d2c9e5eb8aa264c1c1668e5e440249d14
koekoesh <lotfizizaoui@gmail.com>
2023-03-27 15:36:07 +0200

    class Test_makeQuote

--- Test Source/Test_makeQuote.java
+++ Test Source/Test_makeQuote.java
@@ -1,2 +1,18 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class Test_makeQuote {
+
+    @Test
+    public void Test_test(){
+        //Arrange
+
+        //Act
+
+        //Assert
+
+        Assertions.assertEquals("kaas", "kaas");
+    }
 }
