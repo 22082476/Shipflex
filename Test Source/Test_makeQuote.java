5b1651f5da1e9c24b6d131424c7af234788cf02b
koekoesh <lotfizizaoui@gmail.com>
2023-03-29 13:24:11 +0200

    Test run

--- Test Source/Test_makeQuote.java
+++ Test Source/Test_makeQuote.java
@@ -1,18 +1,26 @@
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class Test_makeQuote {
 
+
     @Test
-    public void Test_test(){
+    public void Test_test_zakelijk(){
         //Arrange
+        String typeCustomer = "Zakelijk";
 
         //Act
+        Customer customer = new Customer("Merel Weenstra","Katerstraat", "5655 KB", "Den Haag", 58, 10);
 
         //Assert
-
-        Assertions.assertEquals("kaas", "kaas");
+        assertEquals("Merel Weenstra", customer.getName());
+        assertEquals("Katerstraat", customer.getStreet());
+        assertEquals("5655 KB", customer.getPostcode());
+        assertEquals("Den Haag", customer.getCity());
+        assertEquals(58, customer.getHouseNumber());
+        assertEquals(10, customer.getDiscount());
     }
 }
