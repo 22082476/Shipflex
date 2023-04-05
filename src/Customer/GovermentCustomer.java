9f58f84cc2924d1bbdea834e34343de7ceca6257
Hiresh <22045910@student.hhs.nl>
2023-04-05 15:00:29 +0200

    Fix conflicts

--- src/Customer/GovermentCustomer.java
+++ src/Customer/GovermentCustomer.java
@@ -19,7 +19,7 @@ public class GovermentCustomer extends Customer{
 
     @Override
     public void printCustomer(){
-        super.printCustomer();
         Printer.printLine("Ministerie: " + this.ministry);
+        super.printCustomer();
     }
 }
