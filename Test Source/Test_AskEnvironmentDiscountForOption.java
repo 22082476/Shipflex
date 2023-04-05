e363d295e3d5a939f28fd3f82cb36ca6e505588d
22013024-Romano <22013024@student.hhs.nl>
2023-04-05 10:17:52 +0200

    Test_AskEnvironmentDiscountForOption.java aangemaakt

---
+++ Test Source/Test_AskEnvironmentDiscountForOption.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test_AskEnvironmentDiscountForOption {
+
+    @Test
+    public void testAskEnvironmentDiscountForOption() {
+        // Set up test object
+        Option option = new Option(/* Parameters */);
+
+        // Test valid input
+        setInput("50");
+        askEnvironmentDiscountForOption(option);
+        assertEquals(50, option.getEnvironmentDiscount());
+        assertTrue(getOutput().contains("Je hebt 50% milieukorting toegevoegd aan optie"));
+
+        // Test invalid input (not a number)
+        setInput("not a number");
+        askEnvironmentDiscountForOption(option);
+        assertEquals(50, option.getEnvironmentDiscount());
+        assertTrue(getOutput().contains("Geen nummer ingevuld!"));
+
+        // Test invalid input (out of range)
+        setInput("200");
+        setInput("30");
+        askEnvironmentDiscountForOption(option);
+        assertEquals(30, option.getEnvironmentDiscount());
+        assertTrue(getOutput().contains("Vul een getal in boven 0 en onder de 100!"));
+    }
+}
