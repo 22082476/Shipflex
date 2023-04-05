6ef7de73245a2ff61b5a6688664b8550d6e730f3
Thom <22082476@student.hhs.nl>
2023-04-05 09:57:52 +0200

    Made printer class implementation

--- src/Printer.java
+++ src/Printer.java
@@ -1,7 +1,16 @@
 public class Printer {
 
+public void print(String text){
+    System.out.print(text);
+}
 
+public void printLine(String text){
+    print(String.format(text+"%n"));
+}
 
+public void empytLine(){
+    printLine("");
+}
 
 
     
