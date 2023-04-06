2b227ed3224a9d7216e51216c05c9fa8ec6ccb46
22013024-Romano <22013024@student.hhs.nl>
2023-04-06 13:45:47 +0200

    Interface Print aangemaakt

---
+++ src/DataInOut/Print.java
@@ -0,0 +1,11 @@
+package DataInOut;
+
+public interface Print {
+    void print(String text);
+
+    void printLine(String text);
+
+    void empytLine();
+
+   void printSpaces(int number);
+}
--- src/DataInOut/Printer.java
+++ src/DataInOut/Printer.java
@@ -1,20 +1,20 @@
 package DataInOut;
 
-public class Printer {
+public class Printer implements Print {
 
-public static void print(String text){
+public void print(String text){
     System.out.print(text);
 }
 
-public static void printLine(String text){
+public void printLine(String text){
     print(String.format(text+"%n"));
 }
 
-public static void empytLine(){
+public void empytLine(){
     printLine("");
 }
 
-public static void printSpaces(int number){
+public void printSpaces(int number){
     for(int i = 1; i <= number; i++){
         print(" ");
     }
