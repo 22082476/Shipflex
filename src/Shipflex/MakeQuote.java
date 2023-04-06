8fd55b3ad46e505a3731c9dd20d3be23888b36a3
22013024-Romano <22013024@student.hhs.nl>
2023-04-06 14:57:37 +0200

    MakeQuoteBoat.java gemaakt

--- src/Shipflex/MakeQuote.java
+++ src/Shipflex/MakeQuote.java
@@ -120,93 +120,16 @@ public class MakeQuote {
 
 
 
-    public void selectOption() {
-        List<Integer> validIndexes = Info.printOptionsForBoatType(quote.getBoat().getType());
-
-        String inputString = ScanInput.inputQuestion("de nummer van de optie (stop om te stoppen)");
-
-        if(!ScanInput.ableToParse(inputString)) {
-            if(inputString.equalsIgnoreCase("stop")) {
-                return;
-            }
-            Printer.printLine("Geen nummer ingevuld!");
-            delaySelectOption();
-            return;
-        }
-
-        int optionIndex = Integer.parseInt(inputString);
-
-        if(!validIndexes.contains(optionIndex)) {
-            Printer.printLine("Verkeerde nummer ingevuld!");
-            delaySelectOption();
-        } else if(quote.getBoat().getOptions().contains(Info.getOptions().get(optionIndex))) {
-            Printer.printLine("Deze optie is al toegevoegd!");
-            delaySelectOption();
-        } else {
-            quote.getBoat().addOption(Info.getOptions().get(optionIndex));
-            Printer.printLine("Je hebt optie " + Info.getOptions().get(optionIndex).getName() + " toegevoegd aan de huidige boot!");
-
-            String answer = ScanInput.inputQuestion("ja of nee voor milieukorting");
-
-            if(!answer.equalsIgnoreCase("ja")) {
-                Printer.printLine("Geen milieukorting toegepast voor optie " + Info.getOptions().get(optionIndex).getName());
-                delaySelectOption();
-            } else {
-                askEnvironmentDiscountForOption(Info.getOptions().get(optionIndex));
-            }
-        }
-    }
-
-    public void askEnvironmentDiscountForOption(Option option) {
-        String inputString = ScanInput.inputQuestion("de milieukorting percentage");
-
-        if(!ScanInput.ableToParse(inputString)) {
-            Printer.printLine("Geen nummer ingevuld!");
-            delaySelectOption();
-            return;
-        }
-
-        int discount = Integer.parseInt(inputString);
-
-        if(discount < 1 || discount > 100) {
-            Printer.printLine("Vul een getal in boven 0 en onder de 100!");
-            askEnvironmentDiscountForOption(option);
-            return;
-        }
-
-        option.setEnvironmentDiscount(discount);
-        Printer.printLine("Je hebt " + discount + "%% milieukorting toegevoegd aan optie " + option.getName());
-
-        delaySelectOption();
-    }
-
-    private void delaySelectOption() {
-        try {
-            Thread.sleep(2500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        selectOption();
-    }
 
 
 
 
 
-    private void removeOptionFromBoat() {
-        quote.printOptions(true);
-        int indexInput = ScanInput.inputNumber("de nummer in van de optie die je wilt weghalen");
-        Option option = Info.getOptions().get(indexInput);
 
-        if(!quote.getBoat().getOptions().contains(option)) {
-            Printer.printLine("Die nummer zit niet in de lijst, probeer opnieuw.");
-        } else {
-            quote.getBoat().removeOption(option);
-        }
 
-        Printer.printLine("Je hebt optie " + option.getName() + " weggehaald.");
-    }
+
+
+
 
     public void askWorkHours(){
         int hour = ScanInput.inputNumber("de arbeidsuren");
