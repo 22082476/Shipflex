e0b9803997bc0275f1c03981ed300d412d614750
22013024-Romano <22013024@student.hhs.nl>
2023-04-12 16:33:11 +0200

    test PrintInterface aangemaakt en getinstance toegevoegd

--- Test Source/Test_PrintInterface.java
+++ Test Source/Test_PrintInterface.java
@@ -1,8 +1,7 @@
 import DataInOut.Print;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+
 
 class TestClassPrinterInterface implements Print {
 protected static String outCome = null;
