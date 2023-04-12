993c8603310d249546da227c174298f4691312cf
koekoesh <lotfizizaoui@gmail.com>
2023-04-12 11:36:11 +0200

    arbeidsuren inc en ex

--- src/Shipflex/Quote.java
+++ src/Shipflex/Quote.java
@@ -125,14 +125,14 @@ public class Quote {
 
     public void printOptions(boolean showIndex) {
         for (Option option : this.boat.getOptions()) {
-            if(showIndex)
+            if (showIndex)
                 Info.printOptionInfo(option, Info.getOptions().indexOf(option));
             else
                 Info.printOptionInfo(option, -1);
         }
     }
 
-    public void printCompany(){
+    public void printCompany() {
         Printer.printLine(companyShipbuild.getName());
         Printer.print(companyShipbuild.getStreet());
         Printer.print(String.valueOf(companyShipbuild.getHousenumber()));
@@ -140,7 +140,7 @@ public class Quote {
         Printer.printLine(companyShipbuild.getCity());
     }
 
-    public void printDate(){
+    public void printDate() {
         Printer.printLine("Datum: " + this.date);
         Printer.printLine("Geldigsheid datum: " + this.quoteDate);
     }
@@ -157,7 +157,7 @@ public class Quote {
         Printer.emptyLine();
     }
 
-    public void printQuote(){
+    public void printQuote() {
         printBasicInformation();
         boat.printBoat();
         printOptions(false);
@@ -170,7 +170,19 @@ public class Quote {
         for (Option option : boat.getOptions()) {
             totalPrice += option.getPrice();
         }
+
+        Printer.printLine("Totaal arbeidsUren : " + workHoursCost );
+        Printer.printLine("Totaal arbeidsUren inclusief BTW: " + workHoursCost * 1.09);
         Printer.printLine("Totaal prijs:" + totalPrice);
-        Printer.printLine("Totaal prijs inclusief BTW " + totalPrice *1.21);
+        Printer.printLine("Totaal prijs inclusief BTW " + totalPrice * 1.21);
+
+    }
+
+
+
+
+    public double calculateWorkhourcost () {
+    workHoursCost = workHoursCost / 100 * 9;
+    return workHoursCost;
     }
 }
