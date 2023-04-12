e1f84bbc677b9370638d15a7f023f04bdb34a293
Hiresh <22045910@student.hhs.nl>
2023-04-12 16:28:01 +0200

    Fixes voor plezierjacht

--- src/Boat/PleasureYacht.java
+++ src/Boat/PleasureYacht.java
@@ -5,7 +5,7 @@ public class PleasureYacht extends Boat{
     private String[] optionNames = {"snelheidsmeter", "parapluanker", "binnenverlichting", "roerarm", "yamaha"};
 
     public PleasureYacht() {
-        super("Plezierjacht", "Plezier jacht", 12999.99);
+        super("Plezier Jacht", "plezierjacht", 12999.99);
         super.addOptionsFromNames(optionNames);
     }
 }
