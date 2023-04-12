9c62056a1cf3f2608324f1ca451c76354bcc2037
22013024-Romano <22013024@student.hhs.nl>
2023-04-12 17:01:41 +0200

    Arrange, Act, Assert toegevoegd

--- Test Source/Test_PrintInterface.java
+++ Test Source/Test_PrintInterface.java
@@ -8,46 +8,64 @@ protected static String outCome = null;
 
 @Test
 public void testPrint(){
+    //Arrange
     Print printer = new TestClassPrinterInterface();
     String text = "testen";
+    //Act
     printer.print(text);
+    //Assert
     Assertions.assertEquals(text,outCome);
 }
 
     @Test
     public void testPrintLine() {
+        //Arrange
         Print printer = new TestClassPrinterInterface();
         String text = "testers";
+        //Act
         printer.printLine(text);
+        //Assert
         Assertions.assertEquals(text, outCome);
     }
 
     @Test
     public void testEmptyLine() {
+        //Arrange
         Print printer = new TestClassPrinterInterface();
+        //Act
         printer.emptyLine();
+        //Assert
         Assertions.assertEquals(" <<lege regel>> ", outCome);
     }
 
     @Test
     public void testPrintSpaces() {
+        //Arrange
         Print printer = new TestClassPrinterInterface();
         int number = 5;
+        //Act
         printer.printSpaces(number);
+        //Assert
         Assertions.assertEquals("     ", outCome);
     }
 
     @Test
     public void testPrintTextGenerateQuote() {
+        //Arrange
         Print printer = new TestClassPrinterInterface();
         String[] input = {"Optie 1", "Optie 2", "Optie 3"};
+        //Act
         printer.printTextGenerateQuote(input);
         String expectedOutput = "Commands: [0] Optie 1, [1] Optie 2, [2] Optie 3 Voer een command in: ";
+        //Assert
         Assertions.assertEquals(expectedOutput, outCome);
 
+        //Arrange
         input = new String[]{"Boot 1", "Boot 2", "Boot 3", "Boot 4", "Boot 5"};
+        //Act
         printer.printTextGenerateQuote(input);
         expectedOutput = "Commands: [0] Boot 1, [1] Boot 2, [2] Boot 3, [3] Boot 4, [4] Boot 5 Voer een command in: ";
+        //Assert
         Assertions.assertEquals(expectedOutput, outCome);
     }
 
