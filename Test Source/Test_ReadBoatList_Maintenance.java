0477b4d17b4929e68a6d2b816c923b12d3a1f66c
Thom <22082476@student.hhs.nl>
2023-04-13 12:34:36 +0200

    Changed layout

--- Test Source/Test_ReadBoatList_Maintenance.java
+++ Test Source/Test_ReadBoatList_Maintenance.java
@@ -9,17 +9,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Test_ReadBoatList_Maintenance {
     @Test
     public void testReadBoatList() {
+        //Arrange
         Maintenance maintenance = new Maintenance();
-
         ArrayList<String> expected = new ArrayList<>();
-
         expected.add("yes");
         expected.add("nest");
 
+        //Act
         String inputData = "yes\nnest\nstop";
-
         System.setIn(new ByteArrayInputStream(inputData.getBytes()));
-
         ArrayList<String> result = maintenance.readBoatList("de testje");
 
         assertEquals(expected, result);
