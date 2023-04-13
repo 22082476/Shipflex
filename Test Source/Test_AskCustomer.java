04119ed1a0f9db8d2d49a1bea2c7ff9ad8e7d7ca
koekoesh <lotfizizaoui@gmail.com>
2023-04-13 16:01:49 +0200

    testen customers wijziging

--- Test Source/Test_AskCustomer.java
+++ Test Source/Test_AskCustomer.java
@@ -31,6 +31,13 @@ public class Test_AskCustomer {
 
         //Assert
         assertEquals("naam", MakeQuote.getQuote().getBusinessCustomer().getName());
+        assertEquals("straat", MakeQuote.getQuote().getBusinessCustomer().getStreet());
+        assertEquals("1", MakeQuote.getQuote().getBusinessCustomer().getHouseNumber());
+        assertEquals("2589 EJ", MakeQuote.getQuote().getBusinessCustomer().getPostcode());
+        assertEquals("DenHaag", MakeQuote.getQuote().getBusinessCustomer().getCity());
+        assertEquals("Nederland", MakeQuote.getQuote().getBusinessCustomer().getCountry());
+        assertEquals("0", MakeQuote.getQuote().getBusinessCustomer().getDiscount());
+        assertEquals("ministerie", MakeQuote.getQuote().getBusinessCustomer().getCompany());
 
     }
 
@@ -38,7 +45,7 @@ public class Test_AskCustomer {
 
 
     @ParameterizedTest
-    @CsvSource({"naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, Shipflex, naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, Shipflex,"})
+    @CsvSource({"naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, stichting, naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, stichting,"})
     public void Test_AskCustomerFoundation(String Name, String Street, int Housenumber, String Postcode, String Plaats, String Country, int Percentage, String Company, String expectedName, String expectedStreet, int expectedHousenumber, String expectedPostcode, String expectedPlaats, String expectedCountry, int expectedPercentage, String expectedCompany) {
         //Arange
         MakeQuote makequote = new MakeQuote("rubberboot", new Company("", "", "", "", "", 0));
@@ -53,13 +60,19 @@ public class Test_AskCustomer {
 
         //Assert
         assertEquals("naam", MakeQuote.getQuote().getFoundationCustomer().getName());
-    }
+        assertEquals("straat", MakeQuote.getQuote().getFoundationCustomer().getStreet());
+        assertEquals("1", MakeQuote.getQuote().getFoundationCustomer().getHouseNumber());
+        assertEquals("2589 EJ", MakeQuote.getQuote().getFoundationCustomer().getPostcode());
+        assertEquals("DenHaag", MakeQuote.getQuote().getFoundationCustomer().getCity());
+        assertEquals("Nederland", MakeQuote.getQuote().getFoundationCustomer().getCountry());
+        assertEquals("0", MakeQuote.getQuote().getFoundationCustomer().getDiscount());
+        assertEquals("stichting", MakeQuote.getQuote().getFoundationCustomer().getFoundation());    }
 
 
 
 
     @ParameterizedTest
-    @CsvSource({"naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, Shipflex, naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, Shipflex,"})
+    @CsvSource({"naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, ministerie, naam, straat, 1, 2589 EJ, Den Haag, Nederland, 0, ministerie,"})
     public void Test_AskCustomerGoverment(String Name, String Street, int Housenumber, String Postcode, String Plaats, String Country, int Percentage, String Company, String expectedName, String expectedStreet, int expectedHousenumber, String expectedPostcode, String expectedPlaats, String expectedCountry, int expectedPercentage, String expectedCompany) {
         //Arange
         MakeQuote makequote = new MakeQuote("rubberboot", new Company("", "", "", "", "", 0));
@@ -74,6 +87,13 @@ public class Test_AskCustomer {
 
         //Assert
         assertEquals("naam", MakeQuote.getQuote().getGovermentCustomer().getName());
+        assertEquals("straat", MakeQuote.getQuote().getGovermentCustomer().getStreet());
+        assertEquals("1", MakeQuote.getQuote().getGovermentCustomer().getHouseNumber());
+        assertEquals("2589 EJ", MakeQuote.getQuote().getGovermentCustomer().getPostcode());
+        assertEquals("DenHaag", MakeQuote.getQuote().getGovermentCustomer().getCity());
+        assertEquals("Nederland", MakeQuote.getQuote().getGovermentCustomer().getCountry());
+        assertEquals("0", MakeQuote.getQuote().getGovermentCustomer().getDiscount());
+        assertEquals("ministerie", MakeQuote.getQuote().getGovermentCustomer().getMinistry());
     }
 
 
@@ -94,5 +114,12 @@ public class Test_AskCustomer {
 
         //Assert
         assertEquals("naam", MakeQuote.getQuote().getCustomer().getName());
+        assertEquals("straat", MakeQuote.getQuote().getCustomer().getStreet());
+        assertEquals("1", MakeQuote.getQuote().getCustomer().getHouseNumber());
+        assertEquals("2589 EJ", MakeQuote.getQuote().getCustomer().getPostcode());
+        assertEquals("DenHaag", MakeQuote.getQuote().getCustomer().getCity());
+        assertEquals("Nederland", MakeQuote.getQuote().getCustomer().getCountry());
+        assertEquals("0", MakeQuote.getQuote().getCustomer().getDiscount());
+
     }
 }
