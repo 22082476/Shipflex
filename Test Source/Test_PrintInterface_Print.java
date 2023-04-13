162f0d4514939eed63cc0d19c3a4f4e3b9939a30
Suraj <22040358@student.hhs.nl>
2023-04-13 13:35:15 +0200

    Filenaam gefixt

--- Test Source/Test_PrintInterface_Print.java
+++ Test Source/Test_PrintInterface_Print.java
@@ -1,6 +1,4 @@
 import DataInOut.Print;
-import DataInOut.Printer;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -71,7 +69,7 @@ protected static String outCome = null;
 
 }
 
-class Test_PrinterInterface{
+class Test_PrinterInterface_Print{
     @ParameterizedTest
     @CsvSource({"testen, "})
     public void testPrint(String text, String expected){
