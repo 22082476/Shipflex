d56130235afbc29090fbbca2a86dafd44e54e790
Hiresh <22045910@student.hhs.nl>
2023-04-14 15:02:03 +0200

    Refactor readBoatList

--- src/Shipflex/Maintenance.java
+++ src/Shipflex/Maintenance.java
@@ -6,10 +6,9 @@ import DataInOut.Printer;
 import DataInOut.ScanInput;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Maintenance {
-    private ArrayList<String> boatList = new ArrayList<>();
-
     private String [] commands = {"terug", "optie toevoegen"};
 
     public void start() {
@@ -30,26 +29,24 @@ public class Maintenance {
         }
     }
 
-    public ArrayList<String> readTypeBoatList(String question) {
+    public ArrayList<String> parseBoatTextToList(String question) {
         String userInput = ScanInput.inputQuestion(question);
 
-        if(!userInput.equalsIgnoreCase("stop")) {
-            if (!boatList.equals("")) {
-                boatList.add(userInput);
-                readTypeBoatList(question);
-            }
-        }
+        String[] boatTypes = userInput.split(",|, "); // Split de input zodat de boat types individueel worden opgeslagen en niet in 1 string
+
+        ArrayList<String> boatList = new ArrayList<>(Arrays.asList(boatTypes)); // Voeg ze toe aan de arraylist
 
         return boatList;
     }
 
-        public void addOptionMaintenance(){
-            Info.addOption(new Option(ScanInput.inputQuestion("de naam van het onderdeel"),
-            ScanInput.inputNumberD("de prijs (bijv. 2.99) van het onderdeel"),
-            ScanInput.inputQuestion("categorie van onderdeel"),
-            0,
-            readTypeBoatList("de boot in waarvoor de onderdeel essentieel is (stop om door te gaan)"),
-            readTypeBoatList("de boot in waarvoor de onderdeel optioneel is (stop om door te gaan)")));
-            }
+    public void addOptionMaintenance(){
+        Info.addOption(new Option(
+                ScanInput.inputQuestion("de naam van het onderdeel"),
+                ScanInput.inputNumberD("de prijs (bijv. 2.99) van het onderdeel"),
+                ScanInput.inputQuestion("categorie van onderdeel"),
+                0,
+                parseBoatTextToList("de boten in waarvoor het onderdeel essentieel is (bijv: rubberboot, plezierjacht)"),
+                parseBoatTextToList("de boten in waarvoor het onderdeel optioneel is (bijv: rubberboot, plezierjacht)")));
+    }
 }
 
